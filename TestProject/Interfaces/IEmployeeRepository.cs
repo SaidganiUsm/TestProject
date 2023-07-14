a30b2e7741d828f83b84dbe0c3f8006a80d13662
SaidganiUsm <saidgani110@gmail.com>
2023-07-15 01:32:09 +0500

    Controllers

--- TestProject/Interfaces/IEmployeeRepository.cs
+++ TestProject/Interfaces/IEmployeeRepository.cs
@@ -14,18 +14,5 @@ namespace TestProject.Interfaces
         bool DeleteEmployee(Employee employee);
         bool Save();
         bool IsExist(int id);
-
-        // Many-to-many operations for Project
-        ICollection<Project> GetProjectsByEmployeeId(int employeeId);
-        ICollection<Employee> GetEmployeesByProjectId(int projectId);
-        bool AddEmployeeToProject(int employeeId, int projectId);
-        bool RemoveEmployeeFromProject(int employeeId, int projectId);
-
-
-        // Many-to-many operations for Skill
-        ICollection<Skill> GetSkillsByEmployeeId(int employeeId);
-        ICollection<Employee> GetEmployeesBySkillId(int skillId);
-        bool AddSkillToEmployee(int employeeId, int skillId);
-        bool RemoveSkillFromEmployee(int employeeId, int skillId);
     }
 }
